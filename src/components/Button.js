acfcae82b82e52d191d33b8e1a4df228f4c3798c
vishaljkk <vishal.kk@somaiya.edu>
2022-02-11 22:09:51 +0530

    tutorial 2 code

---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import styles from '../../styles/Button.module.css'
+const Button = () => {
+    return <button type="button" className={styles.prettierButton}>
+        Well bro this is just the begining of our tutorial series
+    </button>   
+}
+
+export default Button;
