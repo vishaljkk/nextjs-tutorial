5815b8bda65a21f76c54aa9cca501cec6e20be05
vishaljkk <vishal.kk@somaiya.edu>
2022-02-12 13:52:34 +0530

    Went over through the getStaticProps

--- pages/messages/index.js
+++ pages/messages/index.js
@@ -1,6 +1,14 @@
-const Sub = () => {
+const Sub = ({message}) => {
     return <div>
-        This is the sub page
+        {message}
     </div>
+}
+// the notFound property is used to redirect to the error page when data is not retrieved from the database
+// There is also a redirection option in the serverside props object
+export const getServerSideProps = (context) => {
+    return { props : {message : "hey whats up"}, 
+            notFound : false} 
+            // notFound : Object ? true : false
+            // redirect : prop here with the destination
 }
 export default Sub;
